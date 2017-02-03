2fa66b4a802bce4908cf4c1b5bed511ace5939de
Andy Hubert <andy.hubert@gmail.com>
2017-02-03 10:14:56 +0200

    Setup google analytics

--- js/biblemesh_helpers.js
+++ js/biblemesh_helpers.js
@@ -80,20 +80,18 @@ biblemesh_Helpers.buildUrlQueryParameters = function(urlpath, overrides) {
     return urlpath + "?" + paramsString;
 };
 
+biblemesh_Helpers.setupGoogleAnalytics = function(gaCode) {
+    // biblemesh_: Google Analytics code below is new
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', gaCode, 'auto');
+}
 
-biblemesh_Helpers.setTimeRelativeToServer = function(callback) {
-    $.ajax({
-        url: location.origin + '/currenttime.json',
-        success: function (result) {
-            serverTimeOffset = result.currentServerTime - new Date().getTime();
-            console.log('Set serverTimeOffset to ' + serverTimeOffset);
-            callback();
-        },
-        error: function (xhr, status, errorThrown) {
-            console.error('Error setting time relative to server.');
-            callback();
-        }
-    });
+biblemesh_Helpers.setServerTimeOffset = function(currentServerTime) {
+    serverTimeOffset = currentServerTime - new Date().getTime();
+    console.log('Set serverTimeOffset to ' + serverTimeOffset);
 }
 
 biblemesh_Helpers.getUTCTimeStamp = function() {
